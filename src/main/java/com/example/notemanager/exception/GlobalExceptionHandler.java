9ff7a47a05deb1babf1eb15c5dec67daa2a8cd08
ruslana <ruslana.prus@ukr.net>
2024-11-28 16:49:19 +0100

    add log

--- src/main/java/com/example/notemanager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/notemanager/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.example.notemanager.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(value = NoteServiceException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleNoteServiceException(NoteServiceException exception, Model model) {
+        log.error("A domain-specific issue was detected: {}", exception.getMessage(), exception);
         model.addAttribute("message", exception.getMessage());
         return "note/error";
     }
@@ -18,6 +23,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(value = Throwable.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public String handleException(Throwable exception, Model model) {
+        log.error("Unexpected error occurred: {}", exception.getMessage(), exception);
         model.addAttribute("message", "Internal Server Error: " + exception.getMessage());
         return "note/error";
     }
